61cdf4d497c8ecd98290393866d3039ce12b59be
damiancampania <damiancampania@gmail.com>
2023-12-13 17:51:27 -0300

    metodo put de edit funcionando (dice post pero es put)

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -10,7 +10,7 @@ router.get("/", adminView);
 router.get("/create", viewAdminCreate);
 router.get("/edit/:id", viewAdminEdit);
 router.post("/create", adminCreate); //mission 6
-router.post("/edit/:id", upload.fields([{ name: 'imagenFront', maxCount: 1 }, { name: 'imagenBack', maxCount: 1 }]), adminEdit);  //mission 6
+router.post("/edit/:id", upload.fields([{ name: 'imagenFront', maxCount: 1 }, { name: 'imagenBack', maxCount: 1 }]), adminEdit);  // es un put
 router.delete("/delete/:id", adminDelete); //mission 6
 
 module.exports = router;
